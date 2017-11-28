7944eb63d898509a61660d439ce6d42138a12e80
吕柏翰 <cptf_lupohan@126.com>
2017-11-28 12:27:55 +0800

    修正注释

--- src/main/java/controller/MeController.java
+++ src/main/java/controller/MeController.java
@@ -36,7 +36,7 @@ public class MeController {
 	 * @author 吕柏翰
 	 * @param code 微信小程序/OAuth2授权的Code
 	 * @param state 微信OAuth2授权的state 对于小程序 值恒为 MiniProgram
-	 * @param success_url 微信OAuth2授权后跳转到的网址
+	 * @param successUrl 微信OAuth2授权后跳转到的网址
 	 * @return String 返回网页的文件名
 	 */
 	@RequestMapping(value="/signin",method=GET)
