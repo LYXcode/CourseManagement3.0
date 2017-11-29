fd7f286570b7c757556fb077c8695644e8cddde1
吕柏翰 <cptf_lupohan@126.com>
2017-11-29 23:17:45 +0800

    Maven运行需要

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,19 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * XXX class
+ * 
+ * @author 吕柏翰
+ * @date 2017/11/29
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Application.class, args);
+	}
+
+}
