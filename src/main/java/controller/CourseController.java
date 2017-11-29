118c56bad3f3e6bbadbfdabddc4e710f2e1f8c7f
吕柏翰 <cptf_lupohan@126.com>
2017-11-29 13:30:03 +0800

    返回类型修改为返回Json数据 并添加Json处理异常的报错

--- src/main/java/controller/CourseController.java
+++ src/main/java/controller/CourseController.java
@@ -4,6 +4,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * CourseController class
  * 课程信息Controller层
@@ -16,39 +19,45 @@ public class CourseController {
 	/**
 	 * 获取与当前用户相关联的课程列表
 	 * @author 吕柏翰
-	 * @return String 返回网页的文件名
+	 * @return String 返回Json数据
+	 * @throws JsonProcessingException Json处理异常
 	 */
+    private ObjectMapper mapper=new ObjectMapper();
+    private Object obj;
 	@RequestMapping(value="/",method=GET)
-	public String getUserCourses(){
-		return "";
+	public String getUserCourses() throws JsonProcessingException{
+		return mapper.writeValueAsString(obj);
 	}
 	/**
 	 * 创建课程
 	 * @author 吕柏翰
-	 * @return String 返回网页的文件名
+	 * @return String 返回Json数据
+	 * @throws JsonProcessingException Json处理异常
 	 */
 	@RequestMapping(value="/",method=POST)
-	public String createCourse(){
-		return "";
+	public String createCourse() throws JsonProcessingException{
+		return mapper.writeValueAsString(obj);
 	}
 	/**
 	 * 按ID获取课程
 	 * @author 吕柏翰
 	 * @param courseId 需获取课程的ID
-	 * @return String 返回网页的文件名
+	 * @return String 返回Json数据
+	 * @throws JsonProcessingException Json处理异常
 	 */
 	@RequestMapping(value="/{courseId}",method=GET)
-	public String getCourseById(Integer courseId){
-		return "";
+	public String getCourseById(Integer courseId) throws JsonProcessingException{
+		return mapper.writeValueAsString(obj);
 	}
 	/**
 	 * 按ID删除课程
 	 * @author 吕柏翰
 	 * @param courseId 需删除课程的ID
-	 * @return String 返回网页的文件名
+	 * @return String 返回Json数据
+	 * @throws JsonProcessingException Json处理异常
 	 */
 	@RequestMapping(value="/{courseId}",method=DELETE)
-	public String deleteCourseById(Integer courseId){
-		return "";
+	public String deleteCourseById(Integer courseId) throws JsonProcessingException{
+		return mapper.writeValueAsString(obj);
 	}
 }
--- src/main/java/controller/MeController.java
+++ src/main/java/controller/MeController.java
@@ -4,6 +4,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * MeController class
  * 用户个人信息Controller层
@@ -16,20 +19,24 @@ public class MeController {
 	/**
 	 * 获取当前用户
 	 * @author 吕柏翰
-	 * @return String 返回网页的文件名
+	 * @return String 返回Json数据
+	 * @throws JsonProcessingException Json处理异常
 	 */
+    private ObjectMapper mapper=new ObjectMapper();
+    private Object obj;
 	@RequestMapping(value="/me",method=GET)
-	public String getCurrentUser(){
-		return "";
+	public String getCurrentUser() throws JsonProcessingException{
+		return mapper.writeValueAsString(obj);
 	}
 	/**
 	 * 修改当前用户
 	 * @author 吕柏翰
-	 * @return String 返回网页的文件名
+	 * @return String 返回Json数据
+	 * @throws JsonProcessingException Json处理异常
 	 */
 	@RequestMapping(value="/me",method=PUT)
-	public String updateCurrentUser(){
-		return "";
+	public String updateCurrentUser() throws JsonProcessingException{
+		return mapper.writeValueAsString(obj);
 	}
 	/**
 	 * 微信小程序/OAuth2登录
@@ -37,10 +44,11 @@ public class MeController {
 	 * @param code 微信小程序/OAuth2授权的Code
 	 * @param state 微信OAuth2授权的state 对于小程序 值恒为 MiniProgram
 	 * @param successUrl 微信OAuth2授权后跳转到的网址
-	 * @return String 返回网页的文件名
+	 * @return String 返回Json数据
+	 * @throws JsonProcessingException Json处理异常
 	 */
 	@RequestMapping(value="/signin",method=GET)
-	public String signinWechat(String code,String state,String successUrl){
-		return "";
+	public String signinWechat(String code,String state,String successUrl) throws JsonProcessingException{
+		return mapper.writeValueAsString(obj);
 	}
 }
