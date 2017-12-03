17e4b897e4dc1d3cb3394f9a86d1f49f10d3936d
吕柏翰 <cptf_lupohan@126.com>
2017-12-03 21:43:37 +0800

    用于返回小组ID

---
+++ src/main/java/entity/GroupID.java
@@ -0,0 +1,19 @@
+package entity;
+
+/**
+ * GroupID class
+ * 小组ID
+ * @author 吕柏翰
+ * @date 2017/12/03
+ */
+public class GroupID {
+	int groupID;
+
+	public int getGroupID() {
+		return groupID;
+	}
+
+	public void setGroupID(int groupID) {
+		this.groupID = groupID;
+	}
+}
