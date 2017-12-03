968f7b80283b7c90d6d1bf024deca54b84dfc83b
Minory Hero <1833731419@qq.com>
2017-12-03 19:41:15 +0800

    实现了部分页面的交互

--- static/js/teacher_class_index.js
+++ static/js/teacher_class_index.js
@@ -9,24 +9,24 @@ var classid=getQueryString("id")[1];
 $.ajax({
     url:"/course/"+courseid,
     type:"GET",
-    success:function(data){
-        $('.courseName').html(data.name);
-        $('.courseIntroduction').html(data.description);
+    success:function(courseData){
+        $('.courseName').html(courseData.name);
+        $('.courseIntroduction').html(courseData.description);
     }
 });
 $.ajax({
 	url:"/class"+classid,
 	type:'GET',
-	success:function(data){
-		$('.title').html(data.name);
-		$('#name').html(data.name);
-		$('#site').html(data.site);
-		$('#time').html(data.time);
-		$('#presentation').html(data.pro.presentation);
-		$('#report').html(data.pro.report);
-		$('#a').html(data.pro.a);
-		$('#b').html(data.pro.b);
-		$('#c').html(data.pro.c);
+	success:function(classData){
+		$('.title').html(classData.name);
+		$('#name').html(classData.name);
+		$('#site').html(classData.site);
+		$('#time').html(classData.time);
+		$('#presentation').html(classData.pro.presentation);
+		$('#report').html(classData.pro.report);
+		$('#a').html(classData.pro.a);
+		$('#b').html(classData.pro.b);
+		$('#c').html(classData.pro.c);
 	}
 });
 
@@ -34,6 +34,25 @@ function onUpdate(){
 	window.location.href="../class/teacher_class_edit?id="+classid;
 };
 
-function onDelete(){
-	
-}
+$(function(){
+	$('.delete_btn').click(function(){
+		if(confirm("确定删除此班？"))
+		{
+			var courseid=$(this).attr('name');
+			
+			$.ajax({
+				url:"/course/"+courseid,
+				method:"DELETE",
+				content:"application/json",
+				success:function(data){
+					alert("删除成功");
+					window.location.href="teacher_index.html";
+				},
+				error:function(data){
+					alert("删除失败");
+					window.location.href="teacher_index.html";
+				}
+			});
+		}
+	});
+	});
